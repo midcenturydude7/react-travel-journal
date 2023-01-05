bb43bfcbda0cc272a28e1ec4784c90a2d1ca739c
midCenturyDude7 <mgriffes@gmail.com>
2023-01-04 22:29:58 -0800

    Added props and loaded card specific data from data.js

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,23 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 
-export default function Card() {
-  
+export default function Card(props) {
+  console.log(props);
   return (
     <div className="card--container">
-      <div className="card--img-container"><img src="https://source.unsplash.com/WLxQvbMyfas" alt="" className="card--img" /></div>
+      <div className="card--img-container"><img src={`${props.card.imageUrl}`} alt="" className="card--img" /></div>
       <div className="card-text--container">
         <div className="card-text--header">
           <span className="card-location-icon"><i className="fa-solid fa-location-dot"></i></span>
-          <span className="card-location-text">Japan</span>
+          <span className="card-location-text">{props.card.location}</span>
           <span className="card-location-map">
-            <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on Google Maps</a>
+            <a href={`${props.card.googleMapsUrl}`}>View on Google Maps</a>
           </span>
         </div>
-        <h1 className="card--text-title">Mount Fuji</h1>
-        <p className="card--text-date">12 Jan, 2021-24 Jan, 2021</p>
-        <p className="card--text-desc">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <h1 className="card--text-title">{props.card.title}</h1>
+        <p className="card--text-date">{props.card.startDate} - {props.card.endDate}</p>
+        <p className="card--text-desc">{props.card.description}</p>
       </div>
     </div>
   );
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Card from "./Card";
+import data from "../Data/data";
 
 export default function Cards() {
   
+  const cards = data.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        card={card}
+      />
+    );
+  });
+
   return (
     <section className="cards-section--container">
-      <Card />
+      {cards}
     </section>
   );
 }
